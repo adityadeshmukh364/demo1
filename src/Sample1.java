a31c9cac658100bb908c31c591b78e2be36b161d
user <user@user-PC>
2024-07-04 17:36:45 +0530

    Created by Aditya:-Sample1 class

---
+++ src/Sample1.java
@@ -0,0 +1,12 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) 
+	{
+	   System.out.println("Welcome to Amravati");
+	   
+	   System.out.println("Welcome to Nagpur");
+
+	}
+
+}
